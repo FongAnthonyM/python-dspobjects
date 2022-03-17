3ed983f9fa6bb39fbec3c390d788a8046e9baf97
Anthony Fong <FongAnthonyM@gmail.com>
2022-03-17 16:41:20 -0700

    Added dataclass objects.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='dspobjects',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='Objects for digital signal processing.',
     long_description='%s\n%s' % (
